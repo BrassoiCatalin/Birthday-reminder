facb68aa225e6183c96131cd8bfbe7c579f07557
Catalin Brassoi <cbrassoi@talentingsoftware.com>
2023-04-10 20:06:32 +0300

    Started implementation for searching by value.

--- src/app/home/birthday-table/birthday-table.component.ts
+++ src/app/home/birthday-table/birthday-table.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { Person } from '../interfaces/person.interface';
 import { PeopleService } from '../services/people.service';
 
+
 @Component({
   selector: 'app-birthday-table',
   templateUrl: './birthday-table.component.html',
   styleUrls: ['./birthday-table.component.scss']
 })
 export class BirthdayTableComponent {
+  visible = false;
+  searchValue = '';
   personList: Person[] = [];
 
   constructor(private peopleService: PeopleService){}
@@ -16,4 +19,15 @@ export class BirthdayTableComponent {
     this.personList = this.peopleService.people;
   }
 
+  reset(): void {
+    this.searchValue = '';
+    this.search();
+  }
+
+  search(): void {
+    this.visible = false;
+    this.personList = this.personList.filter((item: Person) => item.firstName.indexOf(this.searchValue) !== -1);
+  }
+//foloseste doua liste pentru search
+//pune iconita la search, caci e invizibil
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,9 @@ import { HomeRoutingModule } from './home-routing.module';
 import { BirthdayTableComponent } from './birthday-table/birthday-table.component';
 import { NzDividerComponent, NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
@@ -15,7 +18,9 @@ import { NzTableModule } from 'ng-zorro-antd/table';
     CommonModule,
     HomeRoutingModule,
     NzDividerModule,
-    NzTableModule
+    NzTableModule,
+    NzDropDownModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
